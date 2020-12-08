8b00a1335a2a94e58ff529450cbd5f3ece913d6c
Diogo Ferreira <diogodsferreira@gmail.com>
2020-12-08 12:39:57 +0000

    feat: added action to save xmas presents

---
+++ actions/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+NAME_SLOT = os.getenv("name_slot", "PERSON")
+PRESENT_SLOT = os.getenv("present_slot", "product")
--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,32 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from exceptions import SlotNotFound
+from . import NAME_SLOT, PRESENT_SLOT
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def _get_slot(tracker: Tracker, slot: str):
+    slot = tracker.get_slot(slot)
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    if slot is None:
+        raise SlotNotFound(slot)
+
+    return slot
+
+
+class SaveXmasPresent(Action):
+
+    def name(self) -> Text:
+        return "action_save_xmas_present"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = _get_slot(tracker, NAME_SLOT)
+        present = _get_slot(tracker, PRESENT_SLOT)
+
+        return []
